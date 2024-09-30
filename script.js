13d5abf2787aad5fba2fb834491c8ee56f450ba8
tora0001 <tobe.2309@hotmail.com>
2024-09-30 15:45:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,80 @@ window.addEventListener("load", start);
 function start() {
   console.log(`Javascript kører`);
 
+  document.addEventListener("keydown", keyDown);
+
   // start ticking
   tick();
+}
+
+function keyDown(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+    case "a":
+      direction = "left";
+      break;
+    case "ArrowRight":
+    case "d":
+      direction = "right";
+      break;
+    case "ArrowUp":
+    case "w":
+      direction = "up";
+      break;
+    case "ArrowDown":
+    case "s":
+      direction = "down";
+      break;
+  }
 }
 
 function tick() {
   // setup next tick
   setTimeout(tick, 500);
 
-  // TODO: Do stuff
+  for (const part of queue) {
+    writeToCell(part.row, part.col, 0);
+  }
 
+  const head = {
+    row: queue[queue.length - 1].row,
+    col: queue[queue.length - 1].col,
+  };
+
+  switch (direction) {
+    case "left":
+      head.col--;
+      if (head.col < 0) {
+        head.col = 9;
+      }
+      break;
+    case "right":
+      head.col++;
+      if (head.col > 9) {
+        head.col = 0;
+      }
+      break;
+    case "up":
+      head.row--;
+      if (head.row < 0) {
+        head.row = 9;
+      }
+      break;
+    case "down":
+      head.row++;
+      if (head.row > 9) {
+        head.row = 0;
+      }
+      break;
+  }
+
+  queue.push(head);
+
+  queue.shift();
+
+  for (const part of queue) {
+    writeToCell(part.row, part.col, 1);
+  }
   // display the model in full
   displayBoard();
 }
@@ -38,6 +102,23 @@ const model = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
+
+const queue = [
+  {
+    row: 5,
+    col: 7,
+  },
+  {
+    row: 5,
+    col: 6,
+  },
+  {
+    row: 5,
+    col: 5,
+  },
+];
+
+let direction = "left";
 
 function writeToCell(row, col, value) {
   model[row][col] = value;
